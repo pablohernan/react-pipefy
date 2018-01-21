38356730821cf3368c6d9a5e97eba8c997202078
Glitch (profuse-bay) <none>
2018-01-21 02:53:06 +0000

    ☂️🐊 Updated with Glitch

---
+++ client/index.js
@@ -0,0 +1,28 @@
+/* globals PipefyApp */
+import React from 'react';
+import { render } from 'react-dom';
+
+const pipefy = PipefyApp.init();
+
+class ReactSample extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { pipe: null };
+  }
+  componentDidMount() {
+    PipefyApp.render(() => {
+      pipefy.pipe().then((pipe) => {
+        this.setState({ pipe })
+      });
+    });
+  }
+  
+  render() {
+    if (!this.state.pipe) { return <div /> };
+    return <h1>Hello from React on Pipe: {this.state.pipe.name}</h1>
+  }
+}
+
+
+render(<ReactSample />, document.getElementById('application'));
